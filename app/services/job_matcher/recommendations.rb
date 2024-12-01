18764a76d6d07f66a0366b4abca311f452178d59
Fabiano F. Scalco <fabiano@2rk.co>
2024-12-02 08:12:27 +1000

    Refactor to use each_slice

--- app/services/job_matcher/recommendations.rb
+++ app/services/job_matcher/recommendations.rb
@@ -1,25 +1,29 @@
 module JobMatcher
   class Recommendations
-    def self.call
+    def self.call(batch_size: 100)
       jobseekers, jobs = JobMatcher::LoadData.call(
         "./app/files/jobseekers.csv",
         "./app/files/jobs.csv"
       )
       recommendations = []
 
-      jobseekers.each do |jobseeker|
-        jobseeker_skills = jobseeker.skills
-        jobs.each do |job|
-          matching_skills = jobseeker_skills & job.required_skills
-          matching_percent = (matching_skills.size.to_f / job.required_skills.size * 100).round
-          recommendations << Recommendation.new(
-            jobseeker_id: jobseeker.id,
-            jobseeker_name: jobseeker.name,
-            job_id: job.id,
-            job_title: job.title,
-            matching_skill_count: matching_skills.size,
-            matching_skill_percent: matching_percent
-            ) if matching_skills.size > 0
+      jobseekers.each_slice(batch_size) do |jobseeker_batch|
+        jobseeker_batch.each do |jobseeker|
+          jobseeker_skills = jobseeker.skills
+          jobs.each_slice(batch_size) do |job_batch|
+            job_batch.each do |job|
+              matching_skills = jobseeker_skills & job.required_skills
+
+              recommendations << Recommendation.new(
+                jobseeker_id: jobseeker.id,
+                jobseeker_name: jobseeker.name,
+                job_id: job.id,
+                job_title: job.title,
+                matching_skill_count: matching_skills.size,
+                matching_skill_percent: (matching_skills.size.to_f / job.required_skills.size * 100).round
+                ) if matching_skills.size > 0
+            end
+          end
         end
       end
 
